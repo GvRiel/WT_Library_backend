7e2aae2b2566a55fd2347e6077ad63fe1a45906a
FelixvL <digitaleservice@gmail.com>
2022-04-25 15:16:21 +0200

    check

--- src/main/java/nl/workingtalentapp/library/rest/UserHistoryEndpoint.java
+++ src/main/java/nl/workingtalentapp/library/rest/UserHistoryEndpoint.java
@@ -4,6 +4,7 @@ import javax.persistence.GenerationType;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import nl.workingtalentapp.library.domein.UserHistory;
@@ -24,6 +25,10 @@ public class UserHistoryEndpoint {
 	@GetMapping("/userhistory")
 	public Iterable<UserHistory> userhistory() {
 		return uhs.userhistory();
+	}
+	@GetMapping("/testvarfelix/{voornaam}")
+	public void testje(@PathVariable("voornaam") String voornaam) {
+		System.out.println(">>"+voornaam);
 	}
 	
 }
