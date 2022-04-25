dc0e8f820e9c55c9e12fcc7dda27a5730e4c5109
WinnieNH <winniehoogakker@gmail.com>
2022-04-25 13:26:51 +0200

    hoi

--- src/main/java/nl/workingtalentapp/library/domein/Boek.java
+++ src/main/java/nl/workingtalentapp/library/domein/Boek.java
@@ -17,4 +17,42 @@ public class Boek {
 	String title;
 	boolean available;
 	int n_o_copies;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public int getBook_id() {
+		return book_id;
+	}
+	public void setBook_id(int book_id) {
+		this.book_id = book_id;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public boolean isAvailable() {
+		return available;
+	}
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+	public int getN_o_copies() {
+		return n_o_copies;
+	}
+	public void setN_o_copies(int n_o_copies) {
+		this.n_o_copies = n_o_copies;
+	}
+	
+	
 }
