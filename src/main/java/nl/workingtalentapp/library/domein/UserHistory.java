5d56a0504d9bc15bad06302e015bd725e2b177a7
FelixvL <digitaleservice@gmail.com>
2022-04-25 11:26:35 +0200

    alle packages

--- src/main/java/nl/workingtalentapp/library/domein/UserHistory.java
+++ src/main/java/nl/workingtalentapp/library/domein/UserHistory.java
@@ -1,5 +1,15 @@
 package nl.workingtalentapp.library.domein;
 
-public class UserHistory {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class UserHistory {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	long id;
+	
+	int status;
 }
