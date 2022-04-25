9a7661a085028e6db8694a13f6804ce4d918380b
Sofieke N <weraniekolaas@gmail.com>
2022-04-25 11:43:27 +0200

    hoi

--- src/main/java/nl/workingtalentapp/library/domein/Category.java
+++ src/main/java/nl/workingtalentapp/library/domein/Category.java
@@ -1,5 +1,44 @@
 package nl.workingtalentapp.library.domein;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Category {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id; //each defined item here is a column
+	
+	private String cat_name;
+	
+	private boolean main_category;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCat_name() {
+		return cat_name;
+	}
+
+	public void setCat_name(String cat_name) {
+		this.cat_name = cat_name;
+	}
+
+	public boolean isMain_category() {
+		return main_category;
+	}
+
+	public void setMain_category(boolean main_category) {
+		this.main_category = main_category;
+	}
+	
+	
 }
