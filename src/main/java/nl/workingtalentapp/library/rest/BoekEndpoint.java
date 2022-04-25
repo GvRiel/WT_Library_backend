2186813d4f5ec23ec3bf908f49afc2cd974e8dcc
WinnieNH <winniehoogakker@gmail.com>
2022-04-25 16:11:54 +0200

    tekst

--- src/main/java/nl/workingtalentapp/library/rest/BoekEndpoint.java
+++ src/main/java/nl/workingtalentapp/library/rest/BoekEndpoint.java
@@ -2,6 +2,9 @@ package nl.workingtalentapp.library.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import nl.workingtalentapp.library.domein.Boek;
@@ -23,6 +26,18 @@ public class BoekEndpoint {
 	@GetMapping("/boeken")
 	public Iterable<Boek> boeken() {
 		return test.boeken();
+		
+	}
+	
+	@GetMapping("/testvarWinnie/{voornaam}")
+	public void testje(@PathVariable("voornaam") String voornaam) {
+		System.out.println(">>" + voornaam);
+	}
+		
+	@PostMapping("/testpostWinnie/{achternaam}")
+	public void testpost(@RequestBody Boek tes, @PathVariable String achternaam) {
+		System.out.println("van de body " + tes.getTitle());
+		System.out.println("van de path " + achternaam);
 		
 	}
 }
