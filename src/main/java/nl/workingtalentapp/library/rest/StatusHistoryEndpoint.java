d14d6f7b776d8499a6921ad3100143a6461a7a7b
Takoala <64777325+Takoala@users.noreply.github.com>
2022-04-26 15:34:37 +0200

    Class gereproduceerd

--- src/main/java/nl/workingtalentapp/library/rest/StatusHistoryEndpoint.java
+++ src/main/java/nl/workingtalentapp/library/rest/StatusHistoryEndpoint.java
@@ -2,8 +2,13 @@ package nl.workingtalentapp.library.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import nl.workingtalentapp.library.domein.StatusHistory;
+import nl.workingtalentapp.library.domein.UserHistory;
 import nl.workingtalentapp.library.persistance.StatusHistoryService;
 
 @RestController
@@ -20,4 +25,19 @@ public class StatusHistoryEndpoint {
 		return "Hij werkt!";
 	}
 		
+	@GetMapping("/statushistory")
+	public Iterable<StatusHistory> statushistory() {
+		return shs.statushistory();
+	}
+	
+	@GetMapping("/testvarttako/{status_before}")
+	public void testje(@PathVariable("status voor") String status_before) {
+		System.out.println(">>"+status_before);
+	}
+	@PostMapping("/testposttako/{status_after}")
+	public void testpost(@RequestBody StatusHistory sh, @PathVariable String status_after) {
+		System.out.println( "van de body: "+sh.getStatus_after() );
+		System.out.println( "van de path: "+ status_after );
+	}
+	
 }
