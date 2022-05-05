a44c49967928e850b0bf20058de91a04dcfddd29
GvRiel <gijsvriel@hotmail.com>
2022-05-05 13:07:25 +0200

    commit2

---
+++ src/main/java/nl/workingtalentapp/library/persistance/ExemplaarNotFoundException.java
@@ -0,0 +1,8 @@
+package nl.workingtalentapp.library.persistance;
+
+public class ExemplaarNotFoundException extends RuntimeException {
+	public ExemplaarNotFoundException(String message) {
+		super(message);
+	}
+
+}
