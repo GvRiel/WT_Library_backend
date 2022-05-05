63a6220d36da4d56d93a7070b307865d7397a1aa
Takoala <64777325+Takoala@users.noreply.github.com>
2022-05-05 12:17:19 +0200

    Boek domein, service, repo en Endpoint toegevoegd.

--- src/main/java/nl/workingtalentapp/library/domein/Boek.java
+++ src/main/java/nl/workingtalentapp/library/domein/Boek.java
@@ -1,58 +1,151 @@
 package nl.workingtalentapp.library.domein;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Arrays;
+
 
 @Entity
-public class Boek {
+public class Boek implements Serializable {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	long id;
-	
-	int book_id;
-	String isbn;
-	String title;
-	boolean available;
-	int n_o_copies;
-	public long getId() {
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(nullable = false, updatable = false)
+	private Long id;
+	private String title;
+	private String isbn;
+	private String thumbnailUrl;
+	private String authors;
+	private String categories;
+	private String status;
+	private int pageCount;
+	private int copies;
+	private int available;
+
+//	private int n_o_copies;
+//	private String imageUrl;
+
+	@Column(nullable = false, updatable = false)
+	private String bookCode;
+
+	public Boek() {}
+
+	public Boek(Long id, String title, String isbn, String thumbnailUrl, String authors, String categories, String status, int pageCount, int copies, int available, String bookCode) {
+		this.id = id;
+		this.title = title;
+		this.isbn = isbn;
+		this.thumbnailUrl = thumbnailUrl;
+		this.authors = authors;
+		this.categories = categories;
+		this.status = status;
+		this.pageCount = pageCount;
+		this.copies = copies;
+		this.available = available;
+		this.bookCode = bookCode;
+	}
+
+	public Long getId() {
 		return id;
 	}
-	public void setId(long id) {
+
+	public void setId(Long id) {
 		this.id = id;
 	}
-	public int getBook_id() {
-		return book_id;
-	}
-	public void setBook_id(int book_id) {
-		this.book_id = book_id;
-	}
-	public String getIsbn() {
-		return isbn;
-	}
-	public void setIsbn(String isbn) {
-		this.isbn = isbn;
-	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public boolean isAvailable() {
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getThumbnailUrl() {
+		return thumbnailUrl;
+	}
+
+	public void setThumbnailUrl(String thumbnailUrl) {
+		this.thumbnailUrl = thumbnailUrl;
+	}
+
+	public String getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(String authors) {
+		this.authors = authors;
+	}
+
+	public String getCategories() {
+		return categories;
+	}
+
+	public void setCategories(String categories) {
+		this.categories = categories;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public int getPageCount() {
+		return pageCount;
+	}
+
+	public void setPageCount(int pageCount) {
+		this.pageCount = pageCount;
+	}
+
+	public int getCopies() {
+		return copies;
+	}
+
+	public void setCopies(int copies) {
+		this.copies = copies;
+	}
+
+	public int getAvailable() {
 		return available;
 	}
-	public void setAvailable(boolean available) {
+
+	public void setAvailable(int available) {
 		this.available = available;
 	}
-	public int getN_o_copies() {
-		return n_o_copies;
+
+	public String getBookCode() {
+		return bookCode;
 	}
-	public void setN_o_copies(int n_o_copies) {
-		this.n_o_copies = n_o_copies;
+
+	public void setBookCode(String bookCode) {
+		this.bookCode = bookCode;
 	}
-	
-	
+
+	@Override
+	public String toString() {
+		return "Boek{" +
+				"id=" + id +
+				", title='" + title + '\'' +
+				", isbn='" + isbn + '\'' +
+				", thumbnailUrl='" + thumbnailUrl + '\'' +
+				", authors=" + authors +
+				", categories=" + categories +
+				", status='" + status + '\'' +
+				", pageCount=" + pageCount +
+				", copies=" + copies +
+				", available=" + available +
+				", bookCode='" + bookCode + '\'' +
+				'}';
+	}
 }
