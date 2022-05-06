ec7e8830318330179b00aaae97a5f9b0e2d90204
ervandenbosch <ezravdb@gmail.com>
2022-05-06 12:05:08 +0200

    upd User class

--- src/main/java/nl/workingtalentapp/library/persistance/UserRepository.java
+++ src/main/java/nl/workingtalentapp/library/persistance/UserRepository.java
@@ -1,11 +1,16 @@
 package nl.workingtalentapp.library.persistance;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
 
 import nl.workingtalentapp.library.domein.User;
 
 @Component
-public interface UserRepository extends CrudRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, Long>{
+	  void deleteUserById(Long id);
 
+	    Optional<User> findUserById(Long id);	
 }
--- src/main/java/nl/workingtalentapp/library/rest/UserEndpoint.java
+++ src/main/java/nl/workingtalentapp/library/rest/UserEndpoint.java
@@ -1,30 +1,80 @@
 package nl.workingtalentapp.library.rest;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import nl.workingtalentapp.library.domein.User;
 import nl.workingtalentapp.library.persistance.UserService;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins="http://localhost:4200")
+@RequestMapping("/user")
 public class UserEndpoint {
-
-	@Autowired
-	UserService shs;
 	
-	@GetMapping(value = "/newUser")
-	public String eersteUser() {
-		shs.proberen();
-		return "doet het";
+	 private final UserService userService;
+	 public UserEndpoint(UserService UserService) {
+	        this.userService = UserService;
+	    }
+	
+	
+	@GetMapping("/all/dummy")
+	public Iterable<User> ListUsersDummy(){
+		return this.userService.findStaticUsers();
 	}
 	
-	@GetMapping(value = "/users")
+	@GetMapping("/all")
 	public Iterable<User> ListUsers(){
-		return shs.findAllUsers();
-}
+		return this.userService.findAllUsers();
+	}
+	
+	@GetMapping("/find/{username}")
+	public ResponseEntity<User> getUserByUsername(@PathVariable String username){
+		User user = this.userService.findByUsername(username);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+		//return userService.findAll();
+	}
+	
+	@GetMapping("/find/{user_id}/")
+	public ResponseEntity<User> getUserById(@PathVariable("user_id") int user_id){
+	User user = this.userService.findById(user_id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+	}
+
+
+	//DELETE 
+	@DeleteMapping("/delete/{user_id}")
+	public ResponseEntity<Void> deleteUser(
+			 @PathVariable int user_id){
+		
+		//User user = userService.deleteById(id);
+		userService.deleteById(user_id);
+		
+		return ResponseEntity.noContent().build();
+		//return ResponseEntity.notFound().build();
+	}
+	
+
+	//Edit/Update a User
+	//PUT /u
+	  @PutMapping("/update")
+	    public ResponseEntity<User> updateBoek(@RequestBody User user) {
+	        User updateUser = userService.updateUser(user);
+	        return new ResponseEntity<>(updateUser, HttpStatus.OK);
+	  }
+	        
+	@PostMapping("/add")
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        User newUser = userService.addUser(user);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+	}
+	
 }
