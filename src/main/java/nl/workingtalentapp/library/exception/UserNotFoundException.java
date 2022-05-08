f87b7b2fc5bd4486a395360cc1167876c75e05ee
GvRiel <gijsvriel@hotmail.com>
2022-05-08 23:43:47 +0200

    user class gecombineerd

--- src/main/java/nl/workingtalentapp/library/exception/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package nl.workingtalentapp.library.exception;
-
-public class UserNotFoundException extends RuntimeException {
-	public UserNotFoundException(String message) {
-		super(message);
-	}
-
-}
