e80a5b3553dbde7a533ae92fe6edc83bf162621d
Sofieke N <weraniekolaas@gmail.com>
2022-05-09 09:43:43 +0200

    update backend

--- src/main/java/nl/workingtalentapp/library/domein/StatusHistory.java
+++ src/main/java/nl/workingtalentapp/library/domein/StatusHistory.java
@@ -1,5 +1,6 @@
 package nl.workingtalentapp.library.domein;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
@@ -8,100 +9,78 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class StatusHistory {
-@Id
-@GeneratedValue(strategy=GenerationType.AUTO)
-
-long id;
-
-int status_id;
-
-int copy_id;
-
-String title;
-
-boolean admin_modif;
-
-LocalDateTime timestamp;
-
-int user_id;
-
-String status_before;
-
-String status_after;
-
-public long getId() {
-	return id;
-}
-
-public void setId(long id) {
-	this.id = id;
-}
-
-public int getStatus_id() {
-	return status_id;
-}
-
-public void setStatus_id(int status_id) {
-	this.status_id = status_id;
-}
-
-public int getCopy_id() {
-	return copy_id;
-}
-
-public void setCopy_id(int copy_id) {
-	this.copy_id = copy_id;
-}
-
-public String getTitle() {
-	return title;
-}
-
-public void setTitle(String title) {
-	this.title = title;
-}
-
-public boolean isAdmin_modif() {
-	return admin_modif;
-}
-
-public void setAdmin_modif(boolean admin_modif) {
-	this.admin_modif = admin_modif;
-}
-
-public LocalDateTime getTimestamp() {
-	return timestamp;
-}
-
-public void setTimestamp(LocalDateTime timestamp) {
-	this.timestamp = timestamp;
-}
-
-public int getUser_id() {
-	return user_id;
-}
-
-public void setUser_id(int user_id) {
-	this.user_id = user_id;
-}
-
-public String getStatus_before() {
-	return status_before;
-}
-
-public void setStatus_before(String status_before) {
-	this.status_before = status_before;
-}
-
-public String getStatus_after() {
-	return status_after;
-}
-
-public void setStatus_after(String status_after) {
-	this.status_after = status_after;
-}
-
-
+public class StatusHistory implements Serializable{
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	
+	private long copy_id;
+	
+	private long user_id;
+	
+	private boolean admin_modif;
+	private LocalDateTime timestamp;
+	private String status;
+	
+	public StatusHistory() {}
+	
+	public StatusHistory(Long id, Long copy_id, Long user_id, Boolean admin_modif, LocalDateTime timestamp, String status) {
+		this.id = id;
+		this.copy_id = copy_id;
+		this.user_id = user_id;
+		this.admin_modif = admin_modif;
+		this.timestamp = timestamp;
+		this.status = status;
+	}	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public long getCopy_id() {
+		return copy_id;
+	}
+	public void setCopy_id(long copy_id) {
+		this.copy_id = copy_id;
+	}
+	public long getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(long user_id) {
+		this.user_id = user_id;
+	}
+	public boolean isAdmin_modif() {
+		return admin_modif;
+	}
+	public void setAdmin_modif(boolean admin_modif) {
+		this.admin_modif = admin_modif;
+	}
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(LocalDateTime timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	@Override
+	public String toString() {
+		return "StatusHistory{" +
+				"id=" + id +
+				", copy_id='" + copy_id + '\'' +
+				", user_id='" + user_id + '\'' +
+				", admin_modif='" + admin_modif + '\'' +
+				", timestamp=" + timestamp +
+				", status='" + status + '\'' +
+				'}';
+	}
 
 }
